19f2fc9458acb76dfc72f494801a3dd9acc2bedf
Jay T <torrcorp@gmail.com>
2018-06-12 19:27:00 -0600

    added more demonstration classes.

---
+++ src/edu/cnm/deepdive/AssignARef.java
@@ -0,0 +1,48 @@
+package edu.cnm.deepdive;
+
+// Assigning array reference variables.
+class AssignARef {
+
+  public static void main(String[] args) {
+    int i;
+
+    int nums1[] = new int[10];
+    int nums2[] = new int[10];
+
+    for (i = 0; i < 10; i++) {
+      nums1[i] = i;
+    }
+
+    for (i = 0; i < 10; i++) {
+      nums2[i] = -i;
+    }
+    System.out.print("Here is nums1: ");
+    for (i = 0; i < 10; i++) {
+      System.out.print(nums1[i] + " ");
+    }
+    System.out.println();
+
+    System.out.print("Here is nums2: ");
+    for (i = 0; i < 10; i++) {
+      System.out.print(nums2[i] + " ");
+    }
+    System.out.println();
+
+    nums2 = nums1; // now nums 2 refers to nums 1
+
+    System.out.print("Here is nums 2 after assignment: ");
+    for (i = 0; i < 10; i++) {
+      System.out.print(nums2[i] + " ");
+    }
+    System.out.println();
+
+    // now operate on nums 1 array through nums 2
+    nums2[3] = 99;
+
+    System.out.print("Here is nums 1 after change through nums2: ");
+    for (i = 0; i < 10; i++) {
+      System.out.print(nums1[i] + " ");
+    }
+    System.out.println();
+  }
+}
